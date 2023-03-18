30b5c057a78ca6ae760678bcad88d21ee75488b5
Millzy <millzyyt08@gmail.com>
2023-03-18 11:05:29 +0000

    basic stuff

---
+++ Properties/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Slogans")]
+[assembly: AssemblyDescription("Lets you place some customisable text in the environment.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Slogans")]
+[assembly: AssemblyCopyright("Copyright © Millzy 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4B78BA88-7E39-4EF8-94B2-7FB131A84D59")]
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
