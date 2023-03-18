1fcae9cb64ad13a683d403f9c01e7d74228706e7
Millzy <millzyyt08@gmail.com>
2023-03-18 11:06:04 +0000

    app installer

---
+++ Installers/SAppInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Zenject;
+
+namespace Slogans.Installers
+{
+    public class SAppInstaller : Installer
+    {
+        private readonly Config _config;
+        private readonly Plugin _plugin;
+        
+        public SAppInstaller(Config config, Plugin plugin)
+        {
+            _config = config;
+            _plugin = plugin;
+        }
+        
+        public override void InstallBindings()
+        {
+            Container.BindInstance(_config).AsSingle();
+            Container.BindInstance(_plugin).AsSingle();
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,25 @@
+﻿using IPA;
+using IPA.Config.Stores;
+using SiraUtil.Attributes;
+using SiraUtil.Zenject;
+using Slogans.Installers;
+using Conf = IPA.Config.Config;
+using IPALogger = IPA.Logging.Logger;
+
+namespace Slogans
+{
+    [Plugin(RuntimeOptions.DynamicInit), Slog, NoEnableDisable]
+    public class Plugin
+    {
+        [Init]
+        public Plugin(Conf conf, IPALogger logger, Zenjector zenjector)
+        {
+            var config = conf.Generated<Config>();
+            
+            zenjector.UseHttpService();
+            zenjector.UseLogger(logger);
+            
+            zenjector.Install<SAppInstaller>(Location.App, config, this);
+        }
+    }
+}
