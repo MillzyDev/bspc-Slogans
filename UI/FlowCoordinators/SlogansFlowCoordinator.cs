b33aaf3b8c2cde2a9ac1867a1864059aa1db0aed
Millzy <millzyyt08@gmail.com>
2023-03-19 13:39:42 +0000

    non-functioning flow coordinator

---
+++ UI/FlowCoordinators/SlogansFlowCoordinator.cs
@@ -0,0 +1,22 @@
+﻿using HMUI;
+using Zenject;
+
+namespace Slogans.UI.FlowCoordinators
+{
+    public class SlogansFlowCoordinator : FlowCoordinator
+    {
+        protected override void DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
+        {
+            if (!firstActivation) return;
+            
+            SetTitle(nameof(Slogans));
+            showBackButton = true;
+        }
+        
+        [Inject]
+        public void Inject()
+        {
+            
+        }
+    }
+}
